78767c12f5f4f5d7396cb50e7682d6388122e629
Fabian Mastenbroek <mail.fabianm@gmail.com>
2021-02-24 10:53:13 +0100

    Return only latest version when checking releases

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 MAJOR=$(echo $1 | sed -e "s/^v//" -e "s/\.[^.]*$//")
 CURRENT=$(scripts/version.sh -L)
-NEW=$(curl -s https://www.kernel.org/releases.json | jq -r ".releases[]|select(.version | startswith(\"$MAJOR\")) | .version")
+NEW=$(curl -s https://www.kernel.org/releases.json | jq -r ".releases|sort_by(-.released.timestamp)|map(select(.version | startswith(\"$MAJOR\")))|first|.version")
 
 if [[ "$(scripts/version.sh -p)" = "0" ]]; then
     [[ "$(scripts/version.sh -n)" = "$NEW" ]] || echo $NEW
